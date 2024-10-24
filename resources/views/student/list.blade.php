27f541f018da6c2e6b13bc3b92d73735898c2959
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-10-24 15:00:40 +0530

    student

--- resources/views/student/list.blade.php
+++ resources/views/student/list.blade.php
@@ -6,46 +6,47 @@
     </x-slot>
 
     <div class="container mt-4">
-    <a href="{{ url('/students/add') }}" class="btn btn-primary mb-3" style="float: right;">Add New Students</a>
+        <a href="{{ url('/students/add') }}" class="btn btn-primary mb-3 float-right">Add New Students</a>
 
         <!-- Filter Form -->
         <form method="GET" action="{{ route('students.index') }}">
-        @csrf
+            @csrf
             <div class="row mb-4">
                 <div class="col-md-4">
                     <label for="school">School</label>
-                    <select  name="school_id" id="school" class="form-control">
+                    <select name="school_id" id="school" class="form-control">
                         <option value="">All Schools</option>
                         @foreach($schools as $school)
-                            <option value="{{ $school->id }}">
+                            <option value="{{ $school->id }}" {{ request('school_id') == $school->id ? 'selected' : '' }}>
                                 {{ $school->school_name }}
                             </option>
                         @endforeach
                     </select>
                 </div>
-                <div class="col-md-4">
-                    <div>
-                        <label for="standard">Select Standard:</label>
-                        <select name="standard_id" id="standard" class="form-control">
-                            <option value="">Select a Standard</option>
-                        </select>
-                    </div>
-                </div>    
-                    <div class="col-md-4">
-                    <div>
-                        <label for="division">Select Division:</label>
-                        <select name="division_id" id="division" class="form-control">
-                            <option value="">Select a Division</option>
-                        </select>
-                    </div>
-                    </div>
-
 
                 <div class="col-md-4">
-                    <button type="submit" class="btn btn-primary mt-4">Filter</button>
+                    <label for="standard">Select Standard:</label>
+                    <select name="standard_id" id="standard" class="form-control">
+                        <option value="">Select a Standard</option>
+                        <!-- Populated via AJAX -->
+                    </select>
+                </div>
+
+                <div class="col-md-4">
+                    <label for="division">Select Division:</label>
+                    <select name="division_id" id="division" class="form-control">
+                        <option value="">Select a Division</option>
+                        <!-- Populated via AJAX -->
+                    </select>
+                </div>
+
+                <div class="col-md-12 mt-3">
+                    <button type="submit" class="btn btn-primary">Filter</button>
                 </div>
             </div>
         </form>
+
+        <!-- Student List Table -->
         <div class="row">
             <div class="col-12">
                 <div class="table-responsive">
@@ -60,61 +61,67 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($students as $student)
+                            @if($students->isNotEmpty())
+                                @foreach($students as $student)
+                                    <tr>
+                                        <td>{{ $student->roll_no }}</td>
+                                        <td>{{ $student->name }}</td>
+                                        <td>{{ $student->GR_no }}</td>
+                                        <td>{{ $student->uid }}</td>
+                                        <td>{{ $student->division->division_name }}</td>
+                                    </tr>
+                                @endforeach
+                            @else
                                 <tr>
-                                    <td>{{ $student->roll_no }}</td>
-                                    <td>{{ $student->name }}</td>
-                                    <td>{{ $student->GR_no }}</td>
-                                    <td>{{ $student->uid }}</td>
-                                    <td>{{ $student->division->division_name }}</td>
+                                    <td colspan="5" class="text-center">No students found</td>
                                 </tr>
-                            @endforeach
+                            @endif
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
     </div>
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function() {
+            // School change event for fetching standards
             $('#school').change(function() {
                 var schoolId = $(this).val();
-                if(schoolId) {
+                if (schoolId) {
                     $.ajax({
                         url: '{{ url("/get-standards") }}/' + schoolId,
                         type: 'GET',
                         success: function(data) {
-                            $('#standard').empty();
-                            $('#standard').append('<option value="">Select a Standard</option>');
+                            $('#standard').empty().append('<option value="">Select a Standard</option>');
                             $.each(data, function(key, value) {
                                 $('#standard').append('<option value="'+ value.id +'">'+ value.standard_name +'</option>');
                             });
                         }
                     });
                 } else {
-                    $('#standard').empty();
-                    $('#standard').append('<option value="">Select a Standard</option>');
+                    $('#standard').empty().append('<option value="">Select a Standard</option>');
+                    $('#division').empty().append('<option value="">Select a Division</option>');
                 }
             });
 
+            // Standard change event for fetching divisions
             $('#standard').change(function() {
                 var standardId = $(this).val();
-                if(standardId) {
+                if (standardId) {
                     $.ajax({
                         url: '{{ url("/get-divisions") }}/' + standardId,
                         type: 'GET',
                         success: function(data) {
-                            $('#division').empty();
-                            $('#division').append('<option value="">Select a Division</option>');
+                            $('#division').empty().append('<option value="">Select a Division</option>');
                             $.each(data, function(key, value) {
                                 $('#division').append('<option value="'+ value.id +'">'+ value.division_name +'</option>');
                             });
                         }
                     });
                 } else {
-                    $('#division').empty();
-                    $('#division').append('<option value="">Select a Division</option>');
+                    $('#division').empty().append('<option value="">Select a Division</option>');
                 }
             });
         });
