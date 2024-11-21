44f06f52d7eb5976ab371f295c329682a46ff6f3
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-11-21 12:18:14 +0530

    final marksheet generate

--- resources/views/mark/viewfinalmarksheet.blade.php
+++ resources/views/mark/viewfinalmarksheet.blade.php
@@ -1,24 +1,10 @@
-@php
-$studentPercentages = [];
-$rankList = [];
-$rank = 1;
 
-$totalMarksSum = 0;
-$stdmarks = 0;
-$subsubjtotalMarksSum = 0;
-$stdmarkssub = 0;
-$passfail = 0;
-@endphp
- 
-        <div id="pdfContent" style="font-family: system-ui, sans-serif;">
-            @foreach($student as $student_value)
-            @php
-            $index_no = 1; 
-            @endphp
-            @php
-            $studentRank = 1;
-            @endphp
-            <div style="width: 170mm; height: 260mm; padding: 10px; box-sizing: border-box; border: 3px solid black; border-radius: 4px; font-family: Calibri, sans-serif;">
+@foreach($student as $student_value)
+<div style="box-sizing: border-box;"> 
+<div style="font-family: system-ui, sans-serif; ">
+           
+          
+            <div style="width: 170mm; height: 260mm; padding: 10px; border: 3px solid black; border-radius: 4px; font-family: Calibri, sans-serif;">
                 <div style="width: 100%; margin-bottom: 10pt;">
                     <div style="border-radius: 4px; border: 2pt solid black; padding: 15pt; height: 67pt;">
                         <h1 style="text-align: center; font-size: 40pt; font-weight: bold; margin: 0; padding: 0;">{{ ucfirst($student_value['school_name']) }}
@@ -65,57 +51,90 @@ $passfail = 0;
                 <p style="margin: 0;margin-top:10px;">UID - <b>{{ $student_value['uid'] }}</b> </p>
                 <p style="font-size: 16pt; margin: 0; padding: 0px; margin-top: 20px;">Student Name – <b>{{ $student_value['student_name'] }}</b> </p>
 
-                <table style="border-collapse: collapse; width: 100%; margin-top: 10pt;">
-                <div style="text-align: center; margin-bottom: 20px;">
+               <br>
+            
+              
        
-    </div>
-
-    <!-- Main Table -->
-    <table border="1" cellspacing="0" cellpadding="5" style="width: 100%; border-collapse: collapse; text-align: center;">
+    
+   <table border="1" cellspacing="0" cellpadding="5" style="width: 100%; border-collapse: collapse; text-align: center;">
         <thead>
             <tr>
-                <th style="background-color: #f0f0f0;" rowspan="2">Subjects</th>
+                <th style="background-color: #f0f0f0;" >Subjects</th>
                 @if(isset($student_value['exam']))
                 @foreach($student_value['exam'] as $exam_value)
-                <th style="background-color: #f0f0f0;" colspan="2">{{$exam_value['exam_name']}}</th>
+                <th style="background-color: #f0f0f0;" >{{$exam_value['exam_name']}}</th>
                 @endforeach
                 @endif
-                <th style="background-color: #f0f0f0;">Total Marks</th>
-                <th style="background-color: #f0f0f0;">Optain Marks</th>
+                <th style="background-color: #f0f0f0;" >Total Marks</th>
+                <th style="background-color: #f0f0f0;" >Obtain Marks</th>
                 <th style="background-color: #f0f0f0;">sidhi gun</th>
-                <th style="background-color: #f0f0f0;">krupa gun</th>
+                <th style="background-color: #f0f0f0;" >krupa gun</th>
                 <th style="background-color: #f0f0f0;">Grade</th>
                 <th style="background-color: #f0f0f0;">Percentage</th>
-
             </tr>
-            <tr>
            
-            </tr>
+            
+           
         </thead>
         <tbody>
-        @if(isset($student_value['exam']))
-        @foreach($student_value['exam'] as $exam_value)
-          @if(isset($exam_value['subject_Data']))
-           @foreach($exam_value['subject_Data'] as $subject_value)
-            <tr>
-                <td>{{ $subject_value['subject_name']}}</td>
-                    @if(isset($subject_value['marks']))
-                    @foreach($subject_value['marks'] as $mark_value)
-                        <td >{{ $mark_value['marks']}}</td>
-                    @endforeach
-                    @endif
-            </tr>
-            @endforeach
-            @endif
-            @endforeach
-            @endif
+        @php
+            $printedSubjects = [];
+        @endphp
 
-        
-            
+        @if(isset($student_value['exam']))
+    @foreach($student_value['exam'] as $exam_value)
+        @if(isset($exam_value['subject_Data']))
+            @foreach($exam_value['subject_Data'] as $subject_value)
+                @if(!in_array($subject_value['subject_id'], $printedSubjects))
+                    <tr>
+                        <td>{{ $subject_value['subject_name'] }}</td>
+
+                        @php
+                            $totalMarks = 0; 
+                            $obtainmarks = 0;
+                        @endphp
+
+                        @foreach($student_value['exam'] as $exam_loop)
+                            @php
+                                $marksFound = false;
+                            @endphp
+                            @if(isset($exam_loop['subject_Data']))
+                                @foreach($exam_loop['subject_Data'] as $exam_subject_value)
+                                    @if($exam_subject_value['subject_id'] == $subject_value['subject_id'])
+                                        @foreach($exam_subject_value['marks'] as $mark_value)
+                                            <td>{{ $mark_value['marks'] }}</td>
+                                            @php
+                                                $obtainmarks += $mark_value['marks']; // Add to total
+                                                $totalMarks += $mark_value['total_marks'];
+                                                $marksFound = true;
+                                            @endphp
+                                        @endforeach
+                                    @endif
+                                @endforeach
+                            @endif
+
+                            @if(!$marksFound)
+                                <td></td>
+                            @endif
+                        @endforeach
+
+                        <td><strong>{{ $totalMarks }}</strong></td>
+                        <td><strong>{{ $obtainmarks }}</strong></td>
+                    </tr>
+
+                    @php
+                        $printedSubjects[] = $subject_value['subject_id']; // Mark subject as printed
+                    @endphp
+                @endif
+            @endforeach
+        @endif
+    @endforeach
+@endif
+
         </tbody>
         <tfoot>
             <tr>
-                <td style="font-weight: bold;">કુલ ગુણ</td>
+                <td style="font-weight: bold;">Total</td>
                 <td colspan="6"></td>
                 <td style="font-weight: bold;">353</td>
                 <td style="font-weight: bold;">424</td>
@@ -123,11 +142,12 @@ $passfail = 0;
             </tr>
         </tfoot>
     </table>
+<div style="margin-top: 20px; text-align: left;">
+<p style="margin: 0;">Date: 04-05-2020</p>
+<p style="margin: 0;">Signature:</p>
+</div>
+</div>
+<div style="page-break-after: always;"></div>
+@endforeach
 
-    <!-- Footer Section -->
-    <div style="margin-top: 20px; text-align: left;">
-        <p style="margin: 0;">Date: 04-05-2020</p>
-        <p style="margin: 0;">Signature:</p>
-    </div>
-            @endforeach
-        </div>
+</div>
