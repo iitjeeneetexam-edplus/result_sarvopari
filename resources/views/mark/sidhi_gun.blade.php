5f9976027fc0bfa8705cec650a3277dcdcc656a5
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-12-05 11:03:06 +0530

    view performance and grace marks

--- resources/views/mark/sidhi_gun.blade.php
+++ resources/views/mark/sidhi_gun.blade.php
@@ -158,9 +158,9 @@
                                         <input type="text" name="performance_mark[]" id="performance_mark{{$subject_value['subject_id']}}" value="0" class="form-control" readonly disabled>@endif</td>
                                         <td>
                                            
-                                           
+                                       
                                             @if($ned)
-                                           <input type="text" name="performance[]" id="performance_mark_label_hidden{{$subject_value['subject_id']}}"   disabled class="form-control">  
+                                           <input type="text" name="performance[]" id="performance_mark_label_hidden{{$subject_value['subject_id']}}" value="{{$performmark}}"  disabled class="form-control">  
                                            <!-- below code controller ma pass karva mate  -->
                                            <input type="hidden" name="performance_get[]" id="performance_mark_label_hidden{{$subject_value['subject_id']}}"    class="form-control">  
                                            @else
@@ -192,7 +192,7 @@
                                                     name="grace[]" 
                                                     id="grace_input{{$subject_value['subject_id']}}" 
                                                     onchange="handleKeyDown(this, {{$subject_value['subject_id']}},{{$ned}})" 
-
+                                                    value="{{$gracemmark}}"
                                                     class="form-control grace-input">
                                                 <!-- <button type="button" class="btn btn-success submit_grace" data-subject-id="{{$subject_value['subject_id']}}">Submit</button> -->
                                               </div></div
